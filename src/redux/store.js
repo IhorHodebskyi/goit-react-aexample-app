3a4e5b4d338aae84e023d7b7cd3a4971eeccb359
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-08-06 13:36:54 +0300

    add

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,16 @@
 // import { createStore } from 'redux';
 // import { devToolsEnhancer } from 'redux-devtools-extension';
-import { tasksReducer, filtersReducer } from './reducer';
+import { tasksReducer } from "./tasksSlice";
+import { filtersReducer } from "./filtersSlice";
 // const enhancer = devToolsEnhancer();
 // export const store = createStore(rootReducer, enhancer);
 
 //=========================================
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: { tasks: tasksReducer, filters: filtersReducer },
+	reducer: {
+		tasks: tasksReducer,
+		filters: filtersReducer,
+	},
 });
